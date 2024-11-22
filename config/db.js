4ae4bf54328745f832048a75dd771545493c0586
nMintC <nmcuongg1302@gmail.com>
2024-11-22 14:00:00 +0700

    Setup MongoDB connection

---
+++ config/db.js
@@ -0,0 +1,19 @@
+// back-end/config/db.js
+import mongoose from "mongoose";
+
+export const connectDB = async () => {
+  const uri = process.env.MONGODB_URI;
+  if (!uri) {
+    console.error("MONGODB_URI is missing. Create .env from .env.example");
+    process.exit(1);
+  }
+  try {
+    await mongoose.connect(uri, {
+      serverSelectionTimeoutMS: 10000
+    });
+    console.log("MongoDB connected");
+  } catch (err) {
+    console.error("MongoDB connection error:", err.message);
+    process.exit(1);
+  }
+};
